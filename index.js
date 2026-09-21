a08f6a87ff01d441171a8e4896fae389afe039e5
vikashagrahari9522-png <vikashagrahari9522@gmail.com>
2026-09-21 09:18:48 +0530

    first commit of langchain

---
+++ index.js
@@ -0,0 +1,31 @@
+import { config } from "dotenv";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import {PromptTemplate} from "@langchain/core/prompts";
+
+config();
+
+const model = new ChatGoogleGenerativeAI({
+  model: "gemini-3.8-flash",
+  apiKey: process.env.GOOGLE_GENAI_API_KEY,
+});
+
+const promptTemplate = PromptTemplate.fromTemplate(`
+  
+  explain {topic} in very simple way like ELI5,
+  make sure to include the core concepts and avoid unnecessary jargon,
+  make the answer as concise as possible,
+  `)
+
+  promptTemplate.pipe(model).invoke({topic:"express"}).then(response => {
+  console.log(response);
+});
+
+const chain = promptTemplate.pipe(model);
+
+chain.invoke({ topic: "express" }).then((response) => {
+  console.log(response.value);
+});
+
+model.invoke("Who are you?").then(response => {
+  console.log(response.content);
+});
